87497def410cee65591217571974c013b7bcedb8
Andrew <50115930+yb-andrew@users.noreply.github.com>
2019-08-30 14:58:52 -0700

    Update variables.tf with newest version number.

--- variables.tf
+++ variables.tf
@@ -113,7 +113,7 @@ variable "yb_download_url" {
 
 variable "yb_version" {
   description = "The version number of YugaByteDB to install"
-  default     = "1.3.0.0"
+  default     = "1.3.2.1"
   type        = "string"
 }
 
