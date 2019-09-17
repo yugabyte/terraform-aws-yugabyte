ed5927b8bf90f174df8aa01c8958988e524f1d98
rao-vasireddy <38476511+rao-vasireddy@users.noreply.github.com>
2019-09-16 18:15:05 -0700

    Update version to 2.0.0.0

--- variables.tf
+++ variables.tf
@@ -113,7 +113,7 @@ variable "yb_download_url" {
 
 variable "yb_version" {
   description = "The version number of YugaByteDB to install"
-  default     = "1.3.2.1"
+  default     = "2.0.0.0"
   type        = "string"
 }
 
