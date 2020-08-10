dfad199a419edd4845e452010046997808ae0fcf
Bhavin Gandhi <bhavin7392@gmail.com>
2020-08-10 12:32:20 +0530

    Remove old interpolation syntax
    - Few of the statements were already up to date according to Terraform
      0.12, this change updates the remaining statements.
    - Pin the aws provider version to 3.x.
    - Run `terraform fmt`.
    
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,60 +1,61 @@
 #########################################################
 #
-# Outputs for the YugaByte terraform module.
+# Outputs for the YugabyteDB Terraform module.
 #
 #########################################################
 
 output "master-ui" {
   sensitive = false
-  value     = "http://${aws_instance.yugabyte_nodes.*.public_ip[0]}:7000"
+  value     = "http://${aws_instance.yugabyte_nodes[0].public_ip}:7000"
 }
 
 output "tserver-ui" {
   sensitive = false
-  value     = "http://${aws_instance.yugabyte_nodes.*.public_ip[0]}:9000"
+  value     = "http://${aws_instance.yugabyte_nodes[0].public_ip}:9000"
 }
-
 
 output "public_ips" {
   sensitive = false
-  value     = "${aws_instance.yugabyte_nodes.*.public_ip}"
+  value     = aws_instance.yugabyte_nodes.*.public_ip
 }
 
 output "private_ips" {
   sensitive = false
-  value     = "${aws_instance.yugabyte_nodes.*.private_ip}"
+  value     = aws_instance.yugabyte_nodes.*.private_ip
 }
 
 output "security_group" {
   sensitive = false
-  value     = "${aws_security_group.yugabyte.id}"
+  value     = aws_security_group.yugabyte.id
 }
 
 output "ssh_user" {
   sensitive = false
-  value = "${var.ssh_user}"
+  value     = var.ssh_user
 }
+
 output "ssh_key" {
   sensitive = false
-  value     = "${var.ssh_private_key}"
+  value     = var.ssh_private_key
 }
 
 output "JDBC" {
-  sensitive =false
-  value     = "postgresql://yugabyte@${aws_instance.yugabyte_nodes.*.public_ip[0]}:5433"
+  sensitive = false
+  value     = "postgresql://yugabyte@${aws_instance.yugabyte_nodes[0].public_ip}:5433"
 }
 
-output "YSQL"{
+output "YSQL" {
   sensitive = false
-  value     = "ysqlsh -U yugabyte -h ${aws_instance.yugabyte_nodes.*.public_ip[0]} -p 5433"
+  value     = "ysqlsh -U yugabyte -h ${aws_instance.yugabyte_nodes[0].public_ip} -p 5433"
 }
 
-output "YCQL"{
+output "YCQL" {
   sensitive = false
-  value     = "ycqlsh ${aws_instance.yugabyte_nodes.*.public_ip[0]} 9042"
+  value     = "ycqlsh ${aws_instance.yugabyte_nodes[0].public_ip} 9042"
 }
 
-output "YEDIS"{
+output "YEDIS" {
   sensitive = false
-  value     = "redis-cli -h ${aws_instance.yugabyte_nodes.*.public_ip[0]} -p 6379"
- }
+  value     = "redis-cli -h ${aws_instance.yugabyte_nodes[0].public_ip} -p 6379"
+}
+
--- variables.tf
+++ variables.tf
@@ -1,124 +1,125 @@
-#########################################################
+##########################################################
 #
-# Default values for creating a YugaByte cluster on AWS.
+# Default values for creating a YugabyteDB cluster on AWS.
 #
-#########################################################
+##########################################################
 
 variable "associate_public_ip_address" {
   description = "Associate public IP address to instances created."
   default     = true
-  type        = "string"
+  type        = string
 }
 
 variable "cluster_name" {
   description = "The name for the cluster (universe) being created."
-  type        = "string"
+  type        = string
 }
 
 variable "custom_security_group_id" {
   description = "Security group to assign to the instances. Example: 'sg-12345'."
   default     = ""
-  type        = "string"
+  type        = string
 }
 
 variable "instance_type" {
   description = "The type of instances to create."
   default     = "c4.xlarge"
-  type        = "string"
+  type        = string
 }
 
 variable "num_instances" {
   description = "Number of instances in the YugaByte cluster."
   default     = "3"
-  type        = "string"
+  type        = string
 }
 
 variable "prefix" {
   description = "Prefix prepended to all resources created."
   default     = "yb-"
-  type        = "string"
+  type        = string
 }
 
 variable "replication_factor" {
   description = "The replication factor for the universe."
   default     = 3
-  type        = "string"
+  type        = string
 }
 
 variable "root_volume_iops" {
   description = "Provisioned IOPS - valid only for 'io1' type."
   default     = 0
-  type        = "string"
+  type        = string
 }
 
 variable "root_volume_size" {
   description = "The volume size in gigabytes."
   default     = "50"
-  type        = "string"
+  type        = string
 }
 
 variable "root_volume_type" {
   description = "The volume type. Must be one of 'gp2' or 'io1'."
   default     = "gp2"
-  type        = "string"
+  type        = string
 }
 
 variable "ssh_keypair" {
   description = "The SSH keypair name to use for the instances."
-  type        = "string"
+  type        = string
 }
 
 variable "ssh_private_key" {
   description = "The private key to use when connecting to the instances."
-  type        = "string"
+  type        = string
 }
 
 variable "ssh_user" {
   description = "The public key to use when connecting to the instances."
-  type        = "string"
+  type        = string
   default     = "centos"
 }
 
 variable "region_name" {
   description = "Region name for AWS. Example: 'us-west-2'"
-  type        = "string"
+  type        = string
 }
 
 variable "availability_zones" {
   description = "List of availability zones to utilize. Example: ['us-west-1a','us-west-1b','us-west-1c']"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "subnet_ids" {
   description = "List of subnets to launch the instances in. Example: ['subnet-12345','subnet-98765']."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "use_public_ip_for_ssh" {
   description = "Flag to control use of public or private ips for ssh."
   default     = "true"
-  type        = "string"
+  type        = string
 }
 
 variable "vpc_id" {
   description = "The VPC ID to create the security groups in."
-  type        = "string"
+  type        = string
 }
 
 variable "yb_download_url" {
   description = "The download location of the YugaByteDB edition"
   default     = "https://downloads.yugabyte.com"
-  type        = "string"
+  type        = string
 }
 
 variable "yb_version" {
   description = "The version number of YugaByteDB to install"
   default     = "2.2.0.0"
-  type        = "string"
+  type        = string
 }
 
 variable "allowed_sources" {
   description = "Add Source IP in Security Group to restrict the traffic"
-  type        = "list"
+  type        = list(string)
   default     = ["0.0.0.0/0"]
 }
+
