dcbbe26fb633ff937dd6b90248dabbf258323da2
YugaByte CI <yugabyte-ci@users.noreply.github.com>
2025-02-28 00:52:33 +0000

    Update the version to 2024.2.2.0

--- variables.tf
+++ variables.tf
@@ -113,7 +113,7 @@ variable "yb_download_url" {
 
 variable "yb_version" {
   description = "The version number of YugaByteDB to install"
-  default     = "2024.2.1.0"
+  default     = "2024.2.2.0"
   type        = string
 }
 
